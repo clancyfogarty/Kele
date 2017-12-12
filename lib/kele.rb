745dd3c3524263345bc69c5ae8a3b50d96ef69ef
clancyfogarty <clancyfogarty@gmail.com>
2017-12-12 11:44:26 -0800

    Initialize/Authorize KeleClient

---
+++ lib/kele.rb
@@ -0,0 +1,13 @@
+require 'httparty'
+
+class Kele
+
+  include HTTParty
+  base_uri 'https://www.bloc.io/api/v1'
+
+  def initialize(email, password)
+    @user_auth_code = self.class.post("/sessions", body: { "email": email, "password": password })
+    puts @user_auth_code.code
+    raise 'Invalid email or password' if @user_auth_code.code != 200
+  end
+end
